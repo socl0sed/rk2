187c292d5b0f95c6a5783f5945c655e35e77dd5f
socl0sed <letuchiy.force@gmail.com>
2024-05-18 15:02:53 +0000

    created a CMakeLists + build + compiling

---
+++ build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable.dir/CompositePattern.cpp.o"
+  "CMakeFiles/MyExecutable.dir/CompositePattern.cpp.o.d"
+  "MyExecutable"
+  "MyExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
